1e5b5fe296f3aea5c5075d3b39a34f29b6a910a0
David Van Der Bokke <davidv@C02KF33RFFT1.group.on>
2015-05-01 14:24:07 -0700

    Removing a whole bunch of BMP code

--- browsermob-proxy/src/main/java/com/groupon/odo/bmp/ProxyServer.java
+++ browsermob-proxy/src/main/java/com/groupon/odo/bmp/ProxyServer.java
@@ -233,9 +233,6 @@ import net.lightbody.bmp.proxy.jetty.jetty.Server;
 import net.lightbody.bmp.proxy.jetty.util.InetAddrPort;
 import net.lightbody.bmp.proxy.util.Log;
 
-import com.groupon.odo.bmp.http.BrowserMobHttpClient;
-import com.groupon.odo.bmp.http.RequestInterceptor;
-
 import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponseInterceptor;
 import org.java_bandwidthlimiter.BandwidthLimiter;
@@ -259,7 +256,6 @@ public class ProxyServer {
     private Server server;
     private int port = -1;
     private InetAddress localHost;
-    private BrowserMobHttpClient client;
     private StreamManager streamManager;
     private HarPage currentPage;
     private BrowserMobProxyHandler handler;
@@ -292,9 +288,7 @@ public class ProxyServer {
         handler = new BrowserMobProxyHandler();
         handler.setJettyServer(server);
         handler.setShutdownLock(new Object());
-        client = new BrowserMobHttpClient(streamManager, requestCounter);
-        client.prepareForBrowser();
-        handler.setHttpClient(client);
+        //handler.setHttpClient(client);
 
         context.addHandler(handler);
 
@@ -319,7 +313,6 @@ public class ProxyServer {
 
     public void stop() throws Exception {
         cleanup();
-        client.shutdown();
         server.stop();
     }
 
@@ -347,215 +340,6 @@ public class ProxyServer {
         } else
         {
             throw new IllegalArgumentException("Must be address of a local adapter");
-        }
-    }
-
-    public Har getHar() {
-        // Wait up to 5 seconds for all active requests to cease before returning the HAR.
-        // This helps with race conditions but won't cause deadlocks should a request hang
-        // or error out in an unexpected way (which of course would be a bug!)
-        boolean success = ThreadUtils.waitFor(new ThreadUtils.WaitCondition() {
-            @Override
-            public boolean checkCondition(long elapsedTimeInMs) {
-                return requestCounter.get() == 0;
-            }
-        }, TimeUnit.SECONDS, 5);
-
-        if (!success) {
-            LOG.warn("Waited 5 seconds for requests to cease before returning HAR; giving up!");
-        }
-
-        return client.getHar();
-    }
-
-    public Har newHar(String initialPageRef) {
-        pageCount = 1;
-
-        Har oldHar = getHar();
-
-        Har har = new Har(new HarLog(CREATOR));
-        client.setHar(har);
-        newPage(initialPageRef);
-
-        return oldHar;
-    }
-
-    public void newPage(String pageRef) {
-        if (pageRef == null) {
-            pageRef = "Page " + pageCount;
-        }
-
-        client.setHarPageRef(pageRef);
-        currentPage = new HarPage(pageRef);
-        client.getHar().getLog().addPage(currentPage);
-
-        pageCount++;
-    }
-
-    public void endPage() {
-        if (currentPage == null) {
-            return;
-        }
-
-        currentPage.getPageTimings().setOnLoad(new Date().getTime() - currentPage.getStartedDateTime().getTime());
-        client.setHarPageRef(null);
-        currentPage = null;
-    }
-
-    public void setRetryCount(int count) {
-        client.setRetryCount(count);
-    }
-
-    public void remapHost(String source, String target) {
-        client.remapHost(source, target);
-    }
-
-    @Deprecated
-    public void addRequestInterceptor(HttpRequestInterceptor i) {
-        client.addRequestInterceptor(i);
-    }
-
-    public void addRequestInterceptor(RequestInterceptor interceptor) {
-        client.addRequestInterceptor(interceptor);
-    }
-
-    @Deprecated
-    public void addResponseInterceptor(HttpResponseInterceptor i) {
-        client.addResponseInterceptor(i);
-    }
-
-    public void addResponseInterceptor(ResponseInterceptor interceptor) {
-        client.addResponseInterceptor(interceptor);
-    }
-
-    public StreamManager getStreamManager() {
-        return streamManager;
-    }
-
-    //use getStreamManager().setDownstreamKbps instead
-    @Deprecated
-    public void setDownstreamKbps(long downstreamKbps) {
-        streamManager.setDownstreamKbps(downstreamKbps);
-        streamManager.enable();
-    }
-
-    //use getStreamManager().setUpstreamKbps instead
-    @Deprecated
-    public void setUpstreamKbps(long upstreamKbps) {
-        streamManager.setUpstreamKbps(upstreamKbps);
-        streamManager.enable();
-    }
-
-    //use getStreamManager().setLatency instead
-    @Deprecated
-    public void setLatency(long latency) {
-        streamManager.setLatency(latency);
-        streamManager.enable();
-    }
-
-    public void setRequestTimeout(int requestTimeout) {
-        client.setRequestTimeout(requestTimeout);
-    }
-
-    public void setSocketOperationTimeout(int readTimeout) {
-        client.setSocketOperationTimeout(readTimeout);
-    }
-
-    public void setConnectionTimeout(int connectionTimeout) {
-        client.setConnectionTimeout(connectionTimeout);
-    }
-
-    public void autoBasicAuthorization(String domain, String username, String password) {
-        client.autoBasicAuthorization(domain, username, password);
-    }
-
-    public void rewriteUrl(String match, String replace) {
-        client.rewriteUrl(match, replace);
-    }
-    
-    public void clearRewriteRules() {
-    	client.clearRewriteRules();
-    }
-
-    public void blacklistRequests(String pattern, int responseCode) {
-        client.blacklistRequests(pattern, responseCode);
-    }
-    
-    public void clearBlacklist() {
-    	client.clearBlacklist();
-    }
-
-    public void whitelistRequests(String[] patterns, int responseCode) {
-        client.whitelistRequests(patterns, responseCode);
-    }
-    
-    public void clearWhitelist() {
-    	client.clearWhitelist();
-    }
-
-    public void addHeader(String name, String value) {
-        client.addHeader(name, value);
-    }
-
-    public void setCaptureHeaders(boolean captureHeaders) {
-        client.setCaptureHeaders(captureHeaders);
-    }
-
-    public void setCaptureContent(boolean captureContent) {
-        client.setCaptureContent(captureContent);
-    }
-    
-    public void setCaptureBinaryContent(boolean captureBinaryContent) {
-        client.setCaptureBinaryContent(captureBinaryContent);
-    }
-
-    public void clearDNSCache() {
-        client.clearDNSCache();
-    }
-
-    public void setDNSCacheTimeout(int timeout) {
-        client.setDNSCacheTimeout(timeout);
-    }
-
-    public void waitForNetworkTrafficToStop(final long quietPeriodInMs, long timeoutInMs) {
-        long start = System.currentTimeMillis();
-        boolean result = ThreadUtils.waitFor(new ThreadUtils.WaitCondition() {
-            @Override
-            public boolean checkCondition(long elapsedTimeInMs) {
-                Date lastCompleted = null;
-                Har har = client.getHar();
-                if (har == null || har.getLog() == null) {
-                    return true;
-                }
-
-                for (HarEntry entry : har.getLog().getEntries()) {
-                    // if there is an active request, just stop looking
-                    if (entry.getResponse().getStatus() < 0) {
-                        return false;
-                    }
-
-                    Date end = new Date(entry.getStartedDateTime().getTime() + entry.getTime());
-                    if (lastCompleted == null) {
-                        lastCompleted = end;
-                    } else if (end.after(lastCompleted)) {
-                        lastCompleted = end;
-                    }
-                }
-                
-                return lastCompleted != null && System.currentTimeMillis() - lastCompleted.getTime() >= quietPeriodInMs;
-            }
-        }, TimeUnit.MILLISECONDS, timeoutInMs);
-        long end = System.currentTimeMillis();
-        long time = (end - start);
-
-        if (!result) {
-            throw new RuntimeException("Timed out after " + timeoutInMs + " ms while waiting for network traffic to stop");
-        }
-    }
-
-    public void setOptions(Map<String, String> options) {
-        if (options.containsKey("httpProxy")) {
-            client.setHttpProxy(options.get("httpProxy"));
         }
     }
 }
