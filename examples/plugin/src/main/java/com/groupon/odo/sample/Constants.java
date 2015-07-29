995ce52646a9d26a282432b210bb6c6982e91b5f
Tiffany Chou <tchou@groupon.com>
2015-07-29 09:57:23 -0700

    Added default/common overrides.

---
+++ examples/plugin/src/main/java/com/groupon/odo/sample/Constants.java
@@ -0,0 +1,18 @@
+package com.groupon.odo.sample;
+
+public class Constants {
+    public static String contentType = "application/json; charset=utf-8";
+    public static String defaultErrorMessage = "\"Testing http %s Error message here\"";
+
+    public static int statusOK = 200;
+    public static int statusCreated = 201;
+    public static int statusFound = 302;
+    public static int statusBadReq = 400;
+    public static int statusUnauthorized = 401;
+    public static int statusForbidden = 403;
+    public static int statusNotFound = 404;
+    public static int statusRequestTimeOut = 408;
+    public static int statusInternalServerError = 500;
+    public static int statusServiceUnavailable = 503;
+
+}
